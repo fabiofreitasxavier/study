6a275fcc168d42b506474904aeaaef0191c19552
fabiofreitasxavier <160540885+fabiofreitasxavier@users.noreply.github.com>
2025-09-23 22:47:03 +0100

    day-08.2

---
+++ javascript/variables-and-constants/variables.js
@@ -0,0 +1,18 @@
+
+// Variables and Constants in JavaScript 
+
+let user = "Fabio";
+console.log("User:", user);
+user = "Carlos"; // reassignment
+console.log("Updated user:", user);
+
+// Constant example
+const country = "Brazil";
+console.log("Country:", country);
+
+// Uncomment below to test error
+// country = "UK"; // ❌ Error
+
+// var (legacy, not recommended)
+var legacy = "old var";
+console.log("Legacy var:", legacy);
